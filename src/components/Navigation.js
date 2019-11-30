e12bc1b82e363a34057de8b1806bb231c81a6681
corey-guy <corey.drees@gmail.com>
2019-11-30 18:23:43 -0500

    added hamburgert menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './Navigation.css'; 
 import { toggleNavigation } from '../redux/actions';
 import { connect } from 'react-redux';
+import NavigationList from './NavigationList';
 
 const Navigation = ({ toggleNavigation }) => {
 	return(
 		<div className='Navigation'>
 			<header className='Navigation-header' 
 					onClick={() => { toggleNavigation() }}>
-				Hey
+					<div className="ham-slice"></div>
+					<div className="ham-slice"></div>
+					<div className="ham-slice"></div>
+				<NavigationList />
 			</header>
 		</div>
 	);
---
+++ src/components/NavigationList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./NavigationList.css"
+
+
+function NavigationList() { 
+
+	return (
+		<div>
+			<div className="Nav-Item">Item 1</div>
+			<div className="Nav-Item">Item 2</div>
+			<div className="Nav-Item">Item 3</div>
+		</div>
+		);
+}
+
+export default NavigationList;
