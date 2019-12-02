40bbd387d26187d247b1ac500e99c3f4e1d55db2
corey-guy <corey.drees@gmail.com>
2019-12-01 23:02:44 -0500

    added month integer to month text function for display

--- src/components/countries/Country.js
+++ src/components/countries/Country.js
@@ -4,11 +4,42 @@ import { connect } from "react-redux";
 const Country = ({ country }) => (
 	<tr className="country-item">
 		<td>{ country.name }</td>
-		<td>{ country.month_visited }</td>
+		<td>{ getMonthText( country.month_visited )}</td>
 		<td>{ country.year_visited }</td>
 		<td>{ country.continent }</td>
 	</tr>
 );
+
+function getMonthText(month) {
+	switch(month) {
+		case 1:
+			return "January";
+		case 2:
+			return "Febuary";
+		case 3:
+			return "March";
+		case 4:
+			return "April";
+		case 5:
+			return "May";
+		case 6:
+			return "June";
+		case 7:
+			return "July";
+		case 8:
+			return "August";
+		case 9:
+			return "September";
+		case 10:
+			return "October";
+		case 11:
+			return "November";
+		case 12:
+			return "December";	
+		default:
+			return "Mystery Month";
+	}
+}
 
 export default connect(
 	null,
