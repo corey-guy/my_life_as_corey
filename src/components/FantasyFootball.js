d21ca0b88aa6111c898a8fd5e61c3963f3d8a66b
n0251780 <Corey.Drees@LibertyMutual.com>
2019-12-02 15:35:56 -0500

    modified fantasy football.js

--- src/components/FantasyFootball.js
+++ src/components/FantasyFootball.js
@@ -6,6 +6,174 @@ export class FantasyFootball extends React.Component {
 			<div>
 				<h1>Fantasy Football</h1>
 				<h2>You're in how many leagues? Well Let's keep track</h2>
+				<table>
+					<tr>
+						<table>
+							<tr>
+								<td>Chaska</td>
+								<td>Founded 2008</td>
+								<td>Highschool Guys</td>
+							</tr>
+							<tr>
+								<td>
+								Standings
+								</td>
+								<td>Team Name</td>
+								<td>Wins</td>
+								<td>Losses</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>*** ****** Only 2.0</td>
+								<td>8</td>
+								<td>4</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>BuildMyGouldWaller</td>
+								<td>8</td>
+								<td>4</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>Stanley Yelnats</td>
+								<td>8</td>
+								<td>4</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>Klay Thompson</td>
+								<td>7</td>
+								<td>5</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>Left Cheek Sneak</td>
+								<td>7</td>
+								<td>5</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>That Yolo Voodoo</td>
+								<td>7</td>
+								<td>5</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>Team Birno</td>
+								<td>6</td>
+								<td>6</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>Brown No Deal</td>
+								<td>6</td>
+								<td>6</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>Koob Little Fingers</td>
+								<td>6</td>
+								<td>6</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>***********</td>
+								<td>5</td>
+								<td>7</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>Little Red Fournette</td>
+								<td>3</td>
+								<td>9</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>Do it</td>
+								<td>1</td>
+								<td>11</td>
+							</tr>
+						</table>
+					</tr>
+					<tr>
+						<table>
+							<tr>
+								<td>Madison, WI</td>
+								<td>Founded 2013</td>
+								<td>College Guys</td>
+							</tr>
+							<tr>
+								<td>
+								Standings
+								</td>
+								<td>Team Name</td>
+								<td>Wins</td>
+								<td>Losses</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>**** ***** & **** *****</td>
+								<td>9</td>
+								<td>3</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>TEAM **** STEVE</td>
+								<td>9</td>
+								<td>3</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>Guy</td>
+								<td>8</td>
+								<td>4</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>Reek's missing ****</td>
+								<td>7</td>
+								<td>5</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>*****</td>
+								<td>6</td>
+								<td>6</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>*******</td>
+								<td>5</td>
+								<td>7</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>ALADEEN</td>
+								<td>5</td>
+								<td>7</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>Bob Stidanko's Team</td>
+								<td>5</td>
+								<td>7</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>#OutOfBusiness</td>
+								<td>3</td>
+								<td>9</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>The LA Golds</td>
+								<td>3</td>
+								<td>9</td>
+							</tr>
+						</table>
+					</tr>
+				</table>
 			</div>
 		);
 	}
